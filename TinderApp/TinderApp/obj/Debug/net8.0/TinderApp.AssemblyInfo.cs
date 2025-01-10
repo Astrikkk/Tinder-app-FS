3b1a2d042a8f8de2a748f289f9eda96acffed334
puzyrko2007 <puzyrko2007@gmail.com>
2025-01-10 18:56:33 +0200

    CRUD 1

--- TinderApp/TinderApp/obj/Debug/net8.0/TinderApp.AssemblyInfo.cs
+++ TinderApp/TinderApp/obj/Debug/net8.0/TinderApp.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c46774238d033a65205638f0adb4b160e4d2aa0")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf80d0b4dcbffdf6a018828815d4bd3fdd46a54")]
+>>>>>>> Stashed changes
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf80d0b4dcbffdf6a018828815d4bd3fdd46a54")]
 >>>>>>> Stashed changes
