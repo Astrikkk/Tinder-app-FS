2d745a6c6712285fc6e144af5bd59dc2c14dd295
Astragor <101702563+Astrikkk@users.noreply.github.com>
2025-03-08 11:55:23 +0100

    fixed profile

--- TinderApp/TinderApp/Mapper/MapperProfile.cs
+++ TinderApp/TinderApp/Mapper/MapperProfile.cs
@@ -4,7 +4,7 @@ using TinderApp.Data.Entities;
 
 namespace TinderApp.Mapper
 {
-    public class MappingProfile : Profile
+    public class MappingProfile : Profile  // Inherit from AutoMapper.Profile
     {
         public MappingProfile()
         {
@@ -13,11 +13,11 @@ namespace TinderApp.Mapper
                     profile.ProfilePhotos != null && profile.ProfilePhotos.Any(photo => photo.IsPrimary)
                         ? $"/images/profiles/{profile.ProfilePhotos.First(photo => photo.IsPrimary).Path}"
                         : "/images/profiles/noimage.jpg"))
-                .ForMember(dto => dto.Gender, opt => opt.MapFrom(profile => profile.Gender != null ? profile.Gender.Name : ""))
-                .ForMember(dto => dto.LookingFor, opt => opt.MapFrom(profile => profile.LookingFor != null ? profile.LookingFor.Name : ""))
-                .ForMember(dto => dto.InterestedIn, opt => opt.MapFrom(profile => profile.InterestedIn != null ? profile.InterestedIn.Name : ""))
-                .ForMember(dto => dto.Interests, opt => opt.MapFrom(profile => profile.Interests != null ? profile.Interests.Select(i => i.Name).ToList() : new List<string>()))
-                .ForMember(dto => dto.SexualOrientation, opt => opt.MapFrom(profile => profile.SexualOrientation != null ? profile.SexualOrientation.Name : ""))
+                .ForMember(dto => dto.Gender, opt => opt.MapFrom(profile => profile.Gender))
+                .ForMember(dto => dto.LookingFor, opt => opt.MapFrom(profile => profile.LookingFor))
+                .ForMember(dto => dto.InterestedIn, opt => opt.MapFrom(profile => profile.InterestedIn))
+                .ForMember(dto => dto.Interests, opt => opt.MapFrom(profile => profile.Interests))
+                .ForMember(dto => dto.SexualOrientation, opt => opt.MapFrom(profile => profile.SexualOrientation))
                 .ForMember(dto => dto.Photos, opt => opt.MapFrom(profile => profile.ProfilePhotos.Select(photo => $"/images/{photo.Path}").ToList()))
                 .ForMember(dto => dto.LikedByUserIds, opt => opt.MapFrom(profile => profile.LikedBy != null ? profile.LikedBy.Select(u => u.Id).ToList() : new List<int>()))
                 .ForMember(dto => dto.MatchedUserIds, opt => opt.MapFrom(profile => profile.Matches != null ? profile.Matches.Select(u => u.Id).ToList() : new List<int>()));
@@ -30,13 +30,13 @@ namespace TinderApp.Mapper
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
             CreateMap<UserProfile, ProfileResponse>()
-                .ForMember(dto => dto.Gender, opt => opt.MapFrom(profile => profile.Gender != null ? profile.Gender.Name : ""))
-                .ForMember(dto => dto.SexualOrientation, opt => opt.MapFrom(profile => profile.SexualOrientation != null ? profile.SexualOrientation.Name : ""))
+                .ForMember(dto => dto.Gender, opt => opt.MapFrom(profile => profile.Gender.Name))
+                .ForMember(dto => dto.SexualOrientation, opt => opt.MapFrom(profile => profile.SexualOrientation.Name))
                 .ForMember(dto => dto.Photos, opt => opt.MapFrom(profile => profile.ProfilePhotos.Select(photo => $"/images/{photo.Path}").ToList()))
-                .ForMember(dto => dto.Interests, opt => opt.MapFrom(profile => profile.Interests != null ? profile.Interests.Select(interest => interest.Name).ToList() : new List<string>()));
+                .ForMember(dto => dto.Interests, opt => opt.MapFrom(profile => profile.Interests.Select(interest => interest.Name).ToList()));
 
             CreateMap<UserProfile, ProfileDetailsDTO>()
-                .ForMember(dest => dest.GenderName, opt => opt.MapFrom(src => src.Gender != null ? src.Gender.Name : ""))
+                .ForMember(dest => dest.GenderName, opt => opt.MapFrom(src => src.Gender.Name))
                 .ForMember(dest => dest.ProfilePhotoPaths, opt => opt.MapFrom(src => src.ProfilePhotos.Select(p => $"/images/{p.Path}")));
         }
     }
