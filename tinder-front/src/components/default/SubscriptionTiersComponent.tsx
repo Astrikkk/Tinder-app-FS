669735773d89bca9002fb49612505cf736be8ab1
Artemko <eliseevartem77@gmail.com>
2025-03-13 22:28:25 +0200

    some fixes
    fix pages

--- tinder-front/src/components/default/SubscriptionTiersComponent.tsx
+++ tinder-front/src/components/default/SubscriptionTiersComponent.tsx
@@ -18,7 +18,7 @@ const SubscriptionTiers = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="subscription-container">
+    <><div className="subscription-container">
       {/* Header */}
       <header className="subscription-tiers-header">
         <div className="logo-menubar">
@@ -63,7 +63,7 @@ const SubscriptionTiers = () => {
             <li>view users</li>
             <li>limited swipes</li>
             <li>match notifications</li>
-            <li>1 super like per month</li> 
+            <li>1 super like per month</li>
           </ul>
         </div>
         <div className="plan-card">
@@ -81,38 +81,40 @@ const SubscriptionTiers = () => {
           <h3 className="plan-title3">Annual Subscription</h3>
           <p className="plan-price">$89.99</p>
           <ul className="plan-description">
-            <li>Everything in Monthly Plan</li> 
-            <li>Exclusive profile badge</li> 
+            <li>Everything in Monthly Plan</li>
+            <li>Exclusive profile badge</li>
             <li>Premium chat features, like exclusive emojis and reactions</li>
             <li>Priority support</li>
           </ul>
         </div>
       </div>
 
-      {/* Footer */}
-      <footer className="subscription-tiers-footer">
-        <div className="logo-text-footer">
-          <img src={sparkiiFooter} alt="Footer Logo" className="sparkii-footer" />
-          <p className="text-footer">
-            © 2025 Sparkii. By creating a Sparkii account, you agree to be bound by our Terms of Use. These terms outline your rights, responsibilities, and the rules governing your use of our platform.
-          </p>
-        </div>
-        <div className="line-footer"><img src={line} alt="Line" /></div>
-        <nav className="site-navigation-elements-footer">
-          <a href="#" className="faq-footer">FAQ</a>
-          <img src={separator} alt="Separator" className="separator" />
-          <a href="#" className="faq-footer">Contact</a>
-          <img src={separator} alt="Separator" className="separator" />
-          <a href="#" className="faq-footer">Privacy</a>
-          <img src={separator} alt="Separator" className="separator" />
-          <a href="#" className="faq-footer"><img src={youtube} alt="Youtube" />Sparkii</a>
-          <img src={separator} alt="Separator" className="separator" />
-          <a href="#" className="faq-footer"><img src={instagram} alt="Instagram" />@sparkii</a>
-          <img src={separator} alt="Separator" className="separator" />
-          <a href="#" className="faq-footer"><img src={tiktok} alt="TikTok" />@sparkii</a>
-        </nav>
-      </footer>
     </div>
+      <div className="main-Footer">
+        {/* Footer */}
+        <footer className="subscription-tiers-footer">
+          <div className="logo-text-footer">
+            <img src={sparkiiFooter} alt="Footer Logo" className="sparkii-footer" />
+            <p className="text-footer">
+              © 2025 Sparkii. By creating a Sparkii account, you agree to be bound by our Terms of Use. These terms outline your rights, responsibilities, and the rules governing your use of our platform.
+            </p>
+          </div>
+          <div className="line-footer"><img src={line} alt="Line" /></div>
+          <nav className="site-navigation-elements-footer">
+            <a href="#" className="faq-footer">FAQ</a>
+            <img src={separator} alt="Separator" className="separator" />
+            <a href="#" className="faq-footer">Contact</a>
+            <img src={separator} alt="Separator" className="separator" />
+            <a href="#" className="faq-footer">Privacy</a>
+            <img src={separator} alt="Separator" className="separator" />
+            <a href="#" className="faq-footer"><img src={youtube} alt="Youtube" />Sparkii</a>
+            <img src={separator} alt="Separator" className="separator" />
+            <a href="#" className="faq-footer"><img src={instagram} alt="Instagram" />@sparkii</a>
+            <img src={separator} alt="Separator" className="separator" />
+            <a href="#" className="faq-footer"><img src={tiktok} alt="TikTok" />@sparkii</a>
+          </nav>
+        </footer>
+      </div></>
   );
 };
 
